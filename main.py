26f0b499c563de9b4920d4638ad1ebede922d448
Tucker <tnemcek@sovrn.com>
2022-12-06 17:36:02 -0700

    pointed at correct bucket, lambda removed, having permission issues

--- main.py
+++ main.py
@@ -9,30 +9,23 @@ from botocore.exceptions import ClientError
 
 DATABASE = 'dd'
 
-# get all device type counts
-#select count(*), device_type from dd.weblog_superset_dt_2022092221 group by device_type;
-
-#group by hour
-#select dt_hour, count(*) from dd.weblog where device_type is null group by dt_hour;
-
-#select from all
-#select count(*) from dd.weblog where device_type is null;
-
-#TODO: What do we actually want to limit it to?
-#TODO: Do we want to randomize this? When do we want our check?
-
 query = """
-select count(*) from dd.weblog_superset_dt_2021102922 where device_type is null;
+select count(*) from dd.weblog_superset_dt_2021102922 where device is null;
 """
-output = "s3://sovrn-prd-ue2-general-data/weblog-superset/dfs-validator-output/"
+output = "s3://aaa-dsol-test-cases/dfs-validator/validator-outputs/"
+#s3://sovrn-data-working-prd/dfs-validator/
+
+session = boto3.Session(
+    aws_access_key_id=settings.AWS_SERVER_PUBLIC_KEY,#NEED THIS
+    aws_secret_access_key='111CzwGSh9U93UQpLLhjtP+5Oj7pmA/hAZ02Nh6d',
+)
 
 s3 = boto3.resource('s3')
 
 client = boto3.client('athena', region_name='us-east-2')
 
-def lambda_handler(event, context):
-    #executes the query and returns {'QueryExecutionId': 'string'}
-    response_query_execution_id = client.start_query_execution( # MAY require ClientRequestToken
+def execute_query(): 
+    response_query_execution_id = client.start_query_execution(
         QueryString = query,
         QueryExecutionContext = {
             'Database' : DATABASE
@@ -42,22 +35,19 @@ def lambda_handler(event, context):
         }
     )
 
-    
-    response_get_query_details=client.get_query_execution(QueryExecutionId=response_query_execution_id['QueryExecutionId'])
+    response_get_query_details = client.get_query_execution(QueryExecutionId = response_query_execution_id['QueryExecutionId'])
 
     status = 'RUNNING'
-    #check for status of query execution every second for 5 seconds. TODO: adjust this based on needs
+     #check for status of query execution every second for 5 seconds. TODO: adjust this based on needs
     iterations = 5
     while(iterations > 0):
-        iterations = iterations - 1
-        #this takes query execution id as input and returns the details of the executed query
-        response_get_query_details = client.get_query_execution(QueryExecutionId = response_query_execution_id['QueryExecutionId'])
-        status = response_get_query_details['QueryExecution']['Status']['State'] #may be QueryExecutions. Might have to specify first result.
+        iterations = iterations -1
+        status = response_get_query_details['QueryExecution']['Status']['State']
         print(status)
         if(status == 'FAILED') or (status == 'CANCELLED'):
             print('ansible job failed')
             return False
-        elif status == 'SUCEEDED' :
+        elif status == 'SUCCEEDED':
             location = response_get_query_details['QueryExecution']['ResultConfiguration']['OutputLocation']
             #returns query results
             response_query_result = client.get_query_results(QueryExecutionId = response_query_execution_id['QueryExecutionId'])
@@ -69,3 +59,5 @@ def lambda_handler(event, context):
             return True
         else:
             time.sleep(1)
+
+execute_query()
