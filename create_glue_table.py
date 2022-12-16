b66bf0f3f4009a5428b8b60ce71f47856cb543aa
Tucker <tnemcek@sovrn.com>
2022-12-16 12:52:04 -0700

    can now get todays dt

--- create_glue_table.py
+++ create_glue_table.py
@@ -1,60 +1,40 @@
 import boto3
+import get_time_values
 
 client = boto3.client('glue')
 
 #end result is: dfs-validator-crawl-tabledt_2022121500
 
-def create_crawler() :
-    response = client.create_crawler(
-        Name='dfs_validator_crawler',
-        Role='df-glue-common-role-prd-ue1',
-        DatabaseName='dd',
-        Description='Crawler for validating Daas-Feeds-Spark',
-        Targets={
-            'S3Targets': [
-                {
-                    'Path': 's3://sovrn-prd-ue2-general-data/weblog-superset/datasource=requests/dt=2022121500/',
-                    'SampleSize': 1
-                }
-            ]
-        },
-        TablePrefix='dfs_validator_glue_table_',
-        SchemaChangePolicy={
-            'UpdateBehavior': 'UPDATE_IN_DATABASE',
-        },
-        Configuration='{ "Version": 1.0, "CrawlerOutput": { "Partitions": { "AddOrUpdateBehavior": "InheritFromTable" } } }'
-    )
-    print('crawler created')
+get_dt_path = get_time_values.get_todays_dt_path()
+print(get_dt_path)
 
-create_crawler()
-
-def start_crawler():
-    response = client.start_crawler(
-        Name='dfs_validator_crawler'
-    )
-    print('crawler started')
-start_crawler()
-
-# def delete_crawler():
-#     response = client.delete_crawler(
-#         Name="dfs_validator_crawler"
-#     )
-#     print('crawler deleted')
-# delete_crawler()
-
-# response = client.update_table(
-#     DatabaseName='dd',
-#     TableInput={
-#         'Name': 'dfs_validator_crawler_',
-#         'Description': 'Table Sales',
-#         'StorageDescriptor': {
-#             'SerdeInfo': {
-#                 'Name': 'OpenCSVSerde',
-#                 'SerializationLibrary': 'org.apache.hadoop.hive.serde2.OpenCSVSerde',
-#                 'Parameters': {
-#                     'separatorChar': ','
+# def create_crawler() :
+#     response = client.create_crawler(
+#         Name='dfs_validator_crawler',
+#         Role='df-glue-common-role-prd-ue1',
+#         DatabaseName='dd',
+#         Description='Crawler for validating Daas-Feeds-Spark',
+#         Targets={
+#             'S3Targets': [
+#                 {
+#                     'Path': 's3://sovrn-prd-ue2-general-data/weblog-superset/datasource=requests/dt=2022121500/',
+#                     'SampleSize': 1
 #                 }
-#             }
-#         }
-#     }
-# )
+#             ]
+#         },
+#         TablePrefix='dfs_validator_glue_table_',
+#         SchemaChangePolicy={
+#             'UpdateBehavior': 'UPDATE_IN_DATABASE',
+#         },
+#         Configuration='{ "Version": 1.0, "CrawlerOutput": { "Partitions": { "AddOrUpdateBehavior": "InheritFromTable" } } }'
+#     )
+#     print('crawler created')
+
+# create_crawler()
+
+# def start_crawler():
+#     response = client.start_crawler(
+#         Name='dfs_validator_crawler'
+#     )
+#     print('crawler started')
+# start_crawler()
---
+++ get_time_values.py
@@ -0,0 +1,12 @@
+from datetime import datetime, timedelta
+import re
+
+def get_todays_dt_path() :
+    timenow = datetime.now()
+    str_time = str(timenow)
+    clean_time = re.sub("[^0-9]","" , str_time)
+    short_time = clean_time [0:8]
+    todays_dt = str('dt=' + short_time + '00/')
+    todays_dt_path = ('s3://sovrn-prd-ue2-general-data/weblog-superset/datasource=requests/' + todays_dt)
+    print ('todays dt path is ' + todays_dt_path)
+    return todays_dt_path
